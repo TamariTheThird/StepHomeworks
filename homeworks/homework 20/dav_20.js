e4768e909b1a39b72b55d005aaee7ff055eb6ac2
TamariTheThird <tamari.bablidze@gmail.com>
2022-06-26 16:46:26 +0400

    js

---
+++ homeworks/homework 20/dav_20.js
@@ -0,0 +1,32 @@
+// ცვლადების შექმნა
+let number = 13;
+let arr = [];
+let string = 'თამარი';
+let bool_tr = true;
+let bool_false = false;
+let obj = {};
+let func = function () {};
+
+// სტრინგი გამოვიტანოთ HTML-ში
+let str = 'JavaScript';
+document.getElementById('square').innerHTML = str;
+
+// გამოაცხადეთ  ორი რიცხვითი მნიშვნელობის ცვლადი , გამოცხადების შემდგომ მიანიჭეთ მნიშვნელობები
+let a, b;
+a = 11;
+b = 13;
+
+// ცვლადებზე  მოახდინეთ  არითმეტიკული მოქმედებები
+// TYPEOF ფუნქციაში დავბეჭდოთ ერთ ერთის ტიპის ცვლადი (CONSOLE.LOG())
+
+let plus = a + b;
+console.log(plus);
+
+let minus = b - a;
+console.log(minus);
+
+let divide = 0 / b;
+console.log(divide);
+
+let multiply = a * b;
+console.log(multiply);
